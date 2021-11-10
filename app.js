3ddc71dfcd2c0556780e4ed4fc1f9647abac0f9a
Wlandry <landrywabo8@gmail.com>
2021-11-10 23:05:28 +0100

    Début de création d'application

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use((req, res, next)=> {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req,res, next)=> {
+	res.status(201);
+	next();
+});
+
+app.use((req, res) => {
+   res.json({message:'Votre requête a bien été reçue !'});
+});
+
+
+
+
+
+
+
+module.exports = app;
