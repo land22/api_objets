f15594713d0dc1b41155bdcf2bf94258021f6706
Wlandry <landrywabo8@gmail.com>
2021-11-12 14:10:52 +0100

    connecxion de l'application à la base de donnée mongodb adlas

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
 const app = express();
+const dotenv = require('dotenv').config();
+
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+mongoose.connect(process.env.LINK,
+      { useNewUrlParser:true,
+        useUnifiedTopology:true })
+.then(()=> console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 app.use((req, res, next) => {
@@ -10,6 +19,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+app.post('/api/stuff',(req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+  	message: 'Oject créé !'
+  });
 });
 
 app.use('/api/stuff', (req, res, next) => {
